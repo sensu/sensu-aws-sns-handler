5e8daf741112d191ee887652a14a8d787b75cde1
Todd Campbell <todd@sensu.io>
2020-05-05 08:07:25 -0700

    updated test
    Signed-off-by: Todd Campbell <todd@sensu.io>

--- main_test.go
+++ main_test.go
@@ -7,13 +7,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestResolveTemplate(t *testing.T) {
+func TestCheckArgs(t *testing.T) {
 	assert := assert.New(t)
 	event := corev2.FixtureEvent("entity1", "check1")
-	event.Check.State = "passing"
-	plugin.Message = "{{.Entity.Name}}/{{.Check.Name}}: {{.Check.State}}"
-	expectedMessage := "entity1/check1: passing"
-	msg, _ := resolveTemplate(plugin.Message, event)
-	assert.Equal(expectedMessage, msg)
+	plugin.TopicARN = "arn:aws:sns:us-west-2:012345678901:sensu-alerts"
+        assert.NoError(checkArgs(event))
 }
-
